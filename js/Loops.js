c5c698bc5a41ea62b2be216f365566c8365eff17
MoNasr2022 <efmnt2020@gmail.com>
2022-08-11 02:07:07 -0400

    JS Loops

--- js/Loops.js
+++ js/Loops.js
@@ -1,4 +1,5 @@
 // While Loops
+/*
 function getRandomNumber(upper){
     return Math.floor(Math.random()*upper )+1;
 }
@@ -8,3 +9,72 @@ while (counter<10){
     console.log(`The random number is ${getRandomNumber(10)}`) ;
     counter+=1
 }
+
+ */
+//--------------------------------
+
+// do-while loop
+/*
+function getRandomNumber(upper) {
+    return Math.floor(Math.random() * upper) + 1;
+
+}
+let counter = 0;
+
+ do {                      //use do--while loop,  if you want your loop to execute at least one time before checking the condition
+     console.log(`The random number is ${getRandomNumber(10)}`) ;
+     counter++;
+
+ }   while(counter < 10);
+
+ */
+//--------------------------------------
+
+// let secret;
+//
+// do{
+//      secret = prompt("What is the secret password?");
+//
+// }while(secret !=='sesame' )
+// alert("You know the secret password. Welcome!");
+//=====================================================================================
+   // for loops
+
+const main =document.querySelector('main')
+let html= '';
+
+
+for (let i = 1; i <= 10; i++){
+    html+= `<div>${i}</div>`
+}
+main.innerHTML =html;
+
+
+//------------------------------------------
+/*
+const secretWord ='TOMATO';
+let message=` Access denied :(`;
+
+for (let i = 5; i>=1; i--){
+    let guess = prompt(`Enter the secret word. You have ${i} tries.`);
+    if (guess.toUpperCase() === secretWord) {
+        message = 'Welcome to the secret loop world'
+        break
+    }
+}
+alert(message)
+
+ */
+//=----------------------------
+
+const tMinus = 10;
+let message = 'Liftoff! 🚀';
+
+for ( let i = tMinus; i >= 1; i-- ) {
+    let status = prompt(`T-Minus ${i}... Continue? (Y/N)`);
+    if ( status === null || status.toLowerCase() === 'n' ) {
+        message = 'Abort launch!';
+        break;
+    }
+}
+alert(message);
